ee94e5808cc62ad842cb4c4868dc15e52afc4bc4
cicdguy <26552821+cicdguy@users.noreply.github.com>
2023-12-05 19:07:11 -0600

    Add man pages and tests

---
+++ R/hello.R
@@ -0,0 +1,9 @@
+#' Greet an entity. Could be a person, an animal, or an alien.
+#'
+#' @param name The name of the entity to greet.
+#' @return A greeting as a string.
+#' @examples
+#' hello("E.T.")
+hello <- function(name) {
+  paste("Hello", name)
+}
